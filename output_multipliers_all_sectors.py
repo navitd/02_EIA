61fd441216312817bb9b9bd5216195a85e7f4c8d
Navit <navitjob@gmail.com>
2025-04-08 14:29:37 -0400

    output_multipliers_all_sectors.py simple model is correct

---
+++ output_multipliers_all_sectors.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+import os
+import time
+import matplotlib.pyplot as plt
+from func_data_upload2 import data_upload
+from func_plot_L import plot_matrix_columns
+from func_clc_L import clc_L
+
+
+def data_exploration_flags(II,household_expenditure,other_final_demand,output,output_of_final_demand,OECD_rough):
+    print( pd.DataFrame({
+        'II plus f total': II.sum(axis=1) + household_expenditure + other_final_demand.sum(axis=1) ,
+        'Output': output
+    }) )
+    print('expected to be zero:')
+    print(pd.DataFrame({
+        'output_of_final_demand': output_of_final_demand
+    }))
+
+    print(f'sum of output:{output.sum()}, bottom right corner of the matrix:{OECD_rough.loc['OUTPUT', 'TOTAL']}, difference expected to be zero:{output.sum()-OECD_rough.loc['OUTPUT', 'TOTAL']}')
+
+def safe_divide(II, output):
+    # Check if there are NaNs in either II or output
+    if II.isna().any().any():
+        raise ValueError("Matrix II contains NaN values.")
+    if output.isna().any():
+        raise ValueError("Output contains NaN values.")
+    
+    # Replace zeros in outputc with NaN to avoid division by zero
+    output_safe = output.replace(0, np.nan)
+    
+    # Divide II by output, handling NaN values (from division by zero)
+    T = II.divide(output_safe, axis=1)
+    
+    # Replace any NaN values (from division by zero) with zero
+    T = T.fillna(0)
+    
+    return T
+
+def clc_output_multipliers(year):
+    # Module 1: Get IO=II, X, GDP, from OECD, wages, compensation for employees and employment from CANSTAT
+    # later move this to a function that receives country and year and uploads the results
+
+    PPP, OECD, simple_II_labels, statcan_sectors_data, sector_description, T97_values =  data_upload(year)
+    # statcan_sector_description is the explanation in words of the codes. 
+    # If I want to look something up in statcan_rough I can use read_statcan
+    # in the files I downloaded from OECD there's no description of the sectors in words.
+    # I should change statcan_sector_description into a dictionary
+
+    II = OECD.loc[simple_II_labels, simple_II_labels]
+    household_expenditure = OECD.loc[simple_II_labels, 'HFCE']
+    final_demand_columns = ['HFCE',	'NPISH',	'GGFC',	'GFCF',	'INVNT',	'DPABR',	'CONS_NONRES',	'EXPO',	'IMPO']
+    other_final_demand = OECD.loc[simple_II_labels, final_demand_columns[1:]] #exluding HFCE - household expenditure
+    total       = OECD.loc[simple_II_labels, 'TOTAL'] #equals to output, this is x
+    GDP         = OECD.loc['VALU', simple_II_labels]
+    output      = OECD.loc['OUTPUT', simple_II_labels]
+    #I don't need to worry bout household_expenditure of GDP or output - they are both 0
+    # but output of GDP is given and should be marked independently
+
+    #single values in OECD:
+    GDP_of_household_expenditure = OECD.loc['VALU', 'HFCE']
+    GDP_of_total_column = OECD.loc['VALU', 'TOTAL'] # total is the output, should be equal to f_row_sums.sum(axis=0)
+    GDP_of_final_demand = OECD.loc['VALU', final_demand_columns]        #this could be added to the rows from the right or to the columns form the bottom
+    output_of_final_demand = OECD.loc['OUTPUT', final_demand_columns]   #probably will not be needed
+
+    # from looking at the numbers:
+    # total = output
+    # II_row_sums + f_row_sums = total = output
+    # household_expenditure is the column added to II to get the closed model
+    # when uploading data for the first time run:
+    # data_exploration_flags(II,household_expenditure,other_final_demand,output,output_of_final_demand,OECD_rough)
+
+    #final demand and value added are not the same at all
+
+    # Modul 2: calculate L and multipliers
+    T = safe_divide(II, output)
+    Ldf, L_minus_I = clc_L(T)
+
+    IIc = II.copy()
+    IIc["HFCE"] = household_expenditure # added a column for closed model
+    IIc.loc['employees_compensation'] = statcan_sectors_data['employees_compensation'] #If I wanted a column I would have written IIC['employees_compensation']
+    # temporary fix, perhaps I should put household_expenditure of employees_compensation to 0
+    IIc.loc['employees_compensation', 'HFCE'] = 0 #T97_values.loc[T97_values['Transaction'] == 'Compensation of employees', 'OBS_VALUE_USD'].values[0]
+    #T is manifestly not HFCE because the numbers are different
+    outputc = output.copy()
+    outputc['HFCE'] = OECD.loc['OUTPUT', 'HFCE']
+    Tc = safe_divide(IIc, outputc)
+    Lcdf, Lc_minus_I = clc_L(Tc)
+
+    return OECD, simple_II_labels, final_demand_columns, Ldf, Lcdf
+
+###############################################               main               #########################
+start_time = time.time()
+print("working directory of output_multipliers_all_sectors.py is: ",os.getcwd())  # Print the current working directory
+
+# predicting output from year1 to year2
+year1 = '2015'
+year2 = '2020'
+_, _, _, Ldf_year1, Lcdf_year1 = clc_output_multipliers(year1)
+
+OECD_year2, simple_II_labels, final_demand_columns, _, _ = clc_output_multipliers(year2)
+
+output_year2 = OECD_year2.loc['OUTPUT', simple_II_labels]
+outputc_year2 = output_year2.copy()
+outputc_year2.loc['HFCE'] = OECD_year2.loc['OUTPUT', 'HFCE']
+fdf_year2 = OECD_year2.loc[simple_II_labels, final_demand_columns].sum(axis=1)
+fcdf_year2 = OECD_year2.loc[simple_II_labels,final_demand_columns[1:]].sum(axis=1)
+fcdf_year2.loc['employees_compensation'] = 0
+
+
+predicted_output_year2_np  = np.round(Ldf_year1.to_numpy() @ fdf_year2.values.reshape(-1, 1), 1)
+predicted_outputc_year2_np = np.round(Lcdf_year1.to_numpy() @ fcdf_year2.values.reshape(-1,1), 1)
+
+predicted_output_year2 = pd.DataFrame(predicted_output_year2_np, index=Ldf_year1.index, columns=['Predicted_Output'])
+predicted_outputc_year2 = pd.DataFrame(predicted_outputc_year2_np, index=Lcdf_year1.index, columns=['Predicted_Output'])
+
+
+simple model is good
+closed modle could be more accurate. check statcan info. output of HFCE etc.
+check output of HFCE and outut of ther final demand
+
+##############################              plotting          #############################
+plt.figure(figsize=(12, 5))
+
+plt.plot(output_year2.index, output_year2, label='Real Output', marker='o')
+plt.plot(predicted_output_year2.index, predicted_output_year2, label='Predicted Output', marker='o')
+
+plt.xlabel('Sectors')
+plt.ylabel('Output Million [USD]')
+plt.title(f'Real and Predicted Output {year2} Based on Year {year1} - Simple Model')
+plt.legend()
+plt.show()
+
+
+plt.figure(figsize=(12, 5))
+
+plt.plot(outputc_year2.index, outputc_year2, label='Real Output', marker='o')
+plt.plot(predicted_outputc_year2.index, predicted_outputc_year2, label='Predicted Output', marker='o')
+
+plt.xlabel('Sectors')
+plt.ylabel('Output Million [USD]')
+plt.title(f'Real and Predicted Output {year2} Based on Year {year1} - Closed Model')
+plt.legend()
+plt.show()
+
+
+
+# the following is an old plotting. only ICT and only T
+'''
+# Modul 3: plotting
+#ICT sectors information
+ICT_sectors = ['ICT - Manufacturing', 'ICT - Wholesaling', 'ICT - Software and computer services', 'ICT - Communications services',
+               'ICT - Software and computer services',	'ICT - Software and computer services']
+# These correspond to the numbers 17	26	33	34	35	38
+OECD_sectors_ICT = ['C26',	'G',	'J58T60',	'J61',	'J62_63',	'M']
+ICT_sectors_dict = {'ICT - Manufacturing': 'C26',
+                    'ICT - Wholesaling': 'G',
+                    'ICT - Software and computer services': ['J58T60', 'J62_63', 'M'],  
+                    'ICT - Communications services': 'J61'}
+# Build sector code to name mapping
+code_to_name = {}
+for name, codes in ICT_sectors_dict.items():
+    if isinstance(codes, list):
+        for code in codes:
+            code_to_name[code] = name
+    else:
+        code_to_name[codes] = name
+
+
+
+##Simple output multipliers: L
+#plot_matrix_columns(
+#    matrix=Ldf,
+#    sectors=OECD_sectors_ICT,
+#    sector_code_to_name=code_to_name,
+#    title=f'Leontief Matrix Column Profiles - output direct+indirect impact, year {year}'
+#)
+
+
+
+plot_matrix_columns(
+    matrix=L_minus_I,
+    sectors=OECD_sectors_ICT,
+    sector_code_to_name=code_to_name,
+    title=f'output indirect impact, year {year}'
+)'
+'''
