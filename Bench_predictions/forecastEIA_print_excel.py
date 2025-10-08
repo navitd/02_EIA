7423829460a65d018ec05e8f8437f9359ef302cb
Navit <navitjob@gmail.com>
2025-10-08 18:53:16 -0400

    starting forecastEIA_print_excel.py this is a long file pdinting out the long calculations

---
+++ Bench_predictions/forecastEIA_print_excel.py
@@ -0,0 +1,7 @@
+#
+# 1. upload worldbank GDP data + ARIMA forecast 16 years ahead
+# 2. predict compensation of employees E vector from GDP
+# 3. predict sectors E
+# 4. upload final demand F from OECD and use ARIMA to forecast forward
+# 5. create input-output tables for future years
+# 6. print all to excel files
