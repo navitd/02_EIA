ea43152c4f77475ad4bd8174dbdbd401f90d51e0
Navit <navitjob@gmail.com>
2025-10-09 12:06:43 -0400

    Esectors_from_Etot05.py calculates dfE, dffc, and uploads dfEtot from excel

--- Bench_predictions/Esectors_from_Etot05.py
+++ Bench_predictions/Esectors_from_Etot05.py
@@ -82,14 +82,10 @@ fixed_sectors = ['A01_02', 'A03', 'B05_06', 'B07_08', 'B09', 'C10T12', 'C13T15',
 ##########################################################################################################################################   
 final_demand_columns = ['HFCE',	'NPISH', 'GGFC',	'GFCF',	'INVNT', 'CONS_NONRES', 'EXPO'] # 'IMPO', 'DPABR', 
 
-dfoutput = pd.DataFrame() # this will hold output by country, year, sector, output
-dfGDP = pd.DataFrame() # this will hold the GDP by country, year, sector, GDP
-dfGDPimpact = pd.DataFrame() # this will hold country, year, buying sector, selling sector, GDPimpact
+
 dfE = pd.DataFrame() # this will hold country, year, buying sector, selling sector, Eimpact
-dfEimpact = pd.DataFrame()
 dffc = pd.DataFrame()
 dfother_final_demand = pd.DataFrame()
-dfTc = pd.DataFrame()
 for country in countries:
     for year in year_range:
         
@@ -106,8 +102,6 @@ for country in countries:
 
         #dfother_final_demand = collecting_year_country_data_matrix(country, year, dfother_final_demand, other_final_demand , 'other_final_demand')
     
-        dfoutput = collecting_year_country_data_vector(country, year, dfoutput, output, 'output')   
-        dfGDP    = collecting_year_country_data_vector(country, year, dfGDP,    GDP, 'GDP')
         dfE = collecting_year_country_data_vector(country, year, dfE, E, 'Employment')
 
     
@@ -146,65 +140,9 @@ for country in countries:
             dfE.loc[(dfE['year'] == year) & (dfE['country'] == 'GBR'), ['sector', 'Employment']]\
             .set_index('sector').reindex(IIc.columns)['Employment']
             
-        IIc.loc['employees_compensation', 'HFCE'] = 0 
-        outputc = output.copy()
-        outputc['HFCE'] = OECDadditional['employees_compensation'].sum()
-        Tc = safe_divide(IIc, outputc)
-        Lcdf, Lc_minus_I = clc_L(Tc)
-
+  
            
-        # 3. calculate multipliers
-        #############################
-        mo = Ldf.sum(axis=0)                       #dollar's worth of outcome per 1 dollar's worth of new final demand
-        moc_trancated = Lcdf.iloc[:-1].sum(axis=0) #dollar's worth of outcome per 1 dollar's worth of new final demand
-
-        # income multipliers mh
-        Ej_by_xj = Tc.iloc[-1,:-1] #hosehold income received per dollar's worth of sector output  
-        income_F_multipliers = Ldf.mul(Ej_by_xj, axis=0) #household income recieved per dollar's worth of secotr final demand
-        # Ej/xj*Ljk - Ljk is how much output was sold from j to k. and j is the sector that paid the salaries, so Ej/xj is used.
-        sum_income_F_multipliers = income_F_multipliers.sum(axis=0) 
-        
-
-        #income multipliers second time
-        Ej_by_xj = Tc.iloc[-1,:]
-        
-        # GDP multipliers
-        GDPc = OECD.loc['VALU', simple_II_labels + ['HFCE']]
-        GDPj_by_xj = safe_divide_vector(GDPc, outputc)
-
-        # summary of multipliers without typeI and typeII - 
-        # 6 multipliers output, income, GDP, X sector2sector X simple model, closed model
-        # all of the closed model multipliers are trancated (the row and column of salaries and final demand are not included)
-        s2s_mo = Ldf                       # direct + indirect effect
-        s2s_moc = Lcdf                     # direct + indirect + iduced effect
-        s2s_mh = Ldf.mul(Ej_by_xj.iloc[ :-1 ], axis=0) 
-        s2s_mhc = Lcdf.mul(Ej_by_xj.rename(index={'HFCE': 'employees_compensation'}), axis=0)
-        s2s_mg =  Ldf.mul(GDPj_by_xj.iloc[ :-1 ], axis=0)    
-        s2s_mgc = Lcdf.mul(GDPj_by_xj.rename(index={'HFCE': 'employees_compensation'}), axis=0)
-        
-
-
-        ###################################################
-        # multipliers: direct, indirect, induced separately
-        ###################################################
-        n = T.shape[0]
-        # direct
-        direct_o = pd.DataFrame(np.eye(n), index=s2s_mo.index, columns=s2s_mo.columns)
-        direct_h = pd.DataFrame(np.zeros((n, n)), index=Ej_by_xj.iloc[:-1].index, columns=Ej_by_xj.iloc[:-1].index)
-        np.fill_diagonal(direct_h.values, Ej_by_xj.values)
-        direct_g = pd.DataFrame(np.zeros((n, n)), index=GDPj_by_xj.iloc[:-1].index, columns=GDPj_by_xj.iloc[:-1].index)
-        np.fill_diagonal(direct_g.values, GDPj_by_xj.values)
-        #indirect
-        indirect_o = s2s_mo - direct_o
-        #Ej_by_xj*L_minus_I = s2s_mh-Ej_by_xj
-        indirect_h  = s2s_mh - direct_h
-        #GDPj_by_xj*L_minus_I = s2s_mg-GDPj_by_xj
-        indirect_g  = s2s_mg - direct_g
-        #induced
-        induced_o = s2s_moc.iloc[:-1,:-1] - s2s_mo
-        induced_h = s2s_mhc.iloc[:-1,:-1] - s2s_mh
-        induced_g = s2s_mgc.iloc[:-1,:-1] - s2s_mg
-
+       
         #################################
         # impacts instead of multipliers
         #################################
